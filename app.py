ac347655627d1b5d877a767c12ee04a82b750486
Ninad <96688293+NinadD26@users.noreply.github.com>
2024-09-03 17:03:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda with LAMBDA with new image from ECR!'
+        'body': 'Hello from Lambda with LAMBDA with new image from ECR!testing again for workflow'
     }
